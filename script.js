2d25f9a74abedd90dcb0417e2d6a2c303938c24d
P0rtAh <paulafangueiro15@gmail.com>
2025-01-23 13:01:44 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const nome = document.getElementById('nome');
-    nome.addEventListener('click', function () {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
+async function loadNavbar() {
+    const response = await fetch("components/navbar.html");
+    const navbarHtml = await response.text();
+    document.getElementById("navbar").innerHTML = navbarHtml;
+  }
+  
+  async function loadFooter() {
+    const response = await fetch("components/footer.html");
+    const navbarHtml = await response.text();
+    document.getElementById("footer").innerHTML = navbarHtml;
+  }
+  
+  loadFooter();
+  loadNavbar();
 
-    
-    const sobre = document.getElementById('sobre');
-    sobre.classList.add('fadeIn');
-
-    
-    const links = document.querySelectorAll('.scroll-link');
-    links.forEach(link => {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-            const target = document.querySelector(this.getAttribute('href'));
-            window.scrollTo({
-                top: target.offsetTop - 50, 
-                behavior: 'smooth'
-            });
-        });
-    });
-});
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    const sobre = document.getElementById('sobre');
-    sobre.classList.add('fadeIn');
-});
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    const sobre = document.getElementById('sobre');
-    sobre.classList.add('fadeIn');
-});
+ 
